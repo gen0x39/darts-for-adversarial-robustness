a6df463c2895667f67ea401b7855e0afe1ea1582
gen0x39 <migesan0319@gmail.com>
2021-10-11 23:40:08 +0900

    fix

--- cnn/train.py
+++ cnn/train.py
@@ -72,7 +72,6 @@ def main():
 
     # genotype
     genotype = eval("genotypes.%s" % args.arch)
-    genotype = eval("genotypes.%s" % "DARTS_SEED2")
     # print(genotype)
     model = Network(CIFAR_CLASSES)
     model = model.cuda()
